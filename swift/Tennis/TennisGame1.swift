792c08e2e25611c245d531da98f4130dcd2be1fa
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-11-25 19:36:44 +0200

    Add scoreDescription function to Rule and pass scores

--- swift/Tennis/TennisGame1.swift
+++ swift/Tennis/TennisGame1.swift
@@ -22,6 +22,7 @@ protocol Rule {
     init(player1: Player, player2: Player)
     
     func isSatisfied(score1: Int, score2: Int) -> Bool
+    func scoreDescription(score1: Int, score2: Int) -> String
 }
 
 extension Rule {
@@ -51,6 +52,17 @@ final class TieRule: Rule {
     }
     
     func isSatisfied(score1: Int, score2: Int) -> Bool { score1 == score2 }
+    
+    func scoreDescription(score1: Int, score2: Int) -> String {
+        let result: String
+        switch player1.score {
+        case 0: result = "Love-All"
+        case 1: result = "Fifteen-All"
+        case 2: result = "Thirty-All"
+        default: result = "Deuce"
+        }
+        return result
+    }
 }
 
 final class WinRule: Rule {
@@ -73,6 +85,14 @@ final class WinRule: Rule {
     }
     
     func isSatisfied(score1: Int, score2: Int) -> Bool { (score1 >= 4 || score2 >= 4) && scoreDifference >= 2 }
+    
+    func scoreDescription(score1: Int, score2: Int) -> String {
+        let result: String
+        let minusResult = player1.score - player2.score
+        if minusResult>=2 { result = "Win for player1" }
+        else { result = "Win for player2" }
+        return result
+    }
 }
 
 final class AdvantageRule: Rule {
@@ -95,6 +115,14 @@ final class AdvantageRule: Rule {
     }
     
     func isSatisfied(score1: Int, score2: Int) -> Bool { (score1 >= 4 || score2 >= 4) && scoreDifference < 2 }
+    
+    func scoreDescription(score1: Int, score2: Int) -> String {
+        let result: String
+        let minusResult = player1.score - player2.score
+        if minusResult==1 { result = "Advantage player1" }
+        else { result = "Advantage player2" }
+        return result
+    }
 }
 
 final class ScoreRule: Rule {
@@ -126,6 +154,23 @@ final class ScoreRule: Rule {
     }
     
     func isSatisfied(score1: Int, score2: Int) -> Bool { true }
+    
+    func scoreDescription(score1: Int, score2: Int) -> String {
+        var tempScore = 0
+        var result = ""
+        for i in 1..<3 {
+            if i==1 { tempScore = player1.score }
+            else { result = "\(result)-"; tempScore = player2.score }
+            switch tempScore {
+            case 0: result = "\(result)Love"
+            case 1: result = "\(result)Fifteen"
+            case 2: result = "\(result)Thirty"
+            case 3: result = "\(result)Forty"
+            default: break
+            }
+        }
+        return result
+    }
 }
 
 class TennisGame1: TennisGame {
@@ -152,7 +197,7 @@ class TennisGame1: TennisGame {
         
     var score: String? {
         if let rule = rules.first(where: { $0.isSatisfied(score1: player1.score, score2: player2.score) }) {
-            return rule.score
+            return rule.scoreDescription(score1: player1.score, score2: player2.score)
         }
         return ""
     }
